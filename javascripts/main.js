add54cf317facd1f4801be3773812e5c9dfb924c
manland <rmaneschi@gmail.com>
2012-12-03 13:20:50 +0100

    add cv page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
-console.log('This would be the main JS file.');
+function filter(category) {
+	var children = document.getElementById('container').getElementsByTagName('span');
+    for (var i = 0; i < children.length; i++) {
+    	alert(children[i].getAttribute("class"))
+      if(children[i].getAttribute("class").indexOf(category) !== -1) {
+        children[i].parentNode.style.display = '';
+      } else {
+        children[i].parentNode.style.display = 'none';
+      }
+    };
+}
