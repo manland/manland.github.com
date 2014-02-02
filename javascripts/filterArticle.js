90c8c128582b3107bb4b23a6a789528cb2d77249
manland <rmaneschi@gmail.com>
2014-02-02 01:23:55 +0100

    add tag system

--- javascripts/filterArticle.js
+++ javascripts/filterArticle.js
@@ -2,17 +2,30 @@
 
   window.onload = function() {
     var check = function check() {
+      var hash = '';
       if(window.location.hash) {
-        var hash = window.location.hash.split('#')[1];
-        var articles = document.getElementsByClassName('article-min');
-        for(var i=0, len=articles.length; i<len; i++) {
-          var categories = articles[i].getElementsByClassName('category');
-          for(var c=0, len2=categories.length; c<len2; c++) {
-            if(categories[c].innerHTML.search(hash) === -1) {
+        hash = window.location.hash.split('#')[1];
+      }
+      var articles = document.getElementsByClassName('article-min');
+      for(var i=0, len=articles.length; i<len; i++) {
+        var found = false;
+        var categories = articles[i].getElementsByClassName('category');
+        for(var c=0, len2=categories.length; c<len2 && found === false; c++) {
+          if(categories[c].innerHTML.search(hash) === -1) {
+            articles[i].style.display = 'none';
+          } else {
+            articles[i].style.display = '';
+            found = true;
+          }
+        }
+        if(found === false) {
+          var tags = articles[i].getElementsByClassName('tag');
+          for(var c=0, len2=tags.length; c<len2 && found === false; c++) {
+            if(tags[c].innerHTML.search(hash) === -1) {
               articles[i].style.display = 'none';
             } else {
               articles[i].style.display = '';
-              c = len2;
+              found = true;
             }
           }
         }
